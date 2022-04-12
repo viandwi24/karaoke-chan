ea33157003e50ab2adc050a4f6464156165b4be8
viandwi24 <viandwicyber@gmail.com>
2022-04-12 17:18:47 +0700

    adding control button for song player

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 // @ts-ignore
+import Lyrics from 'song-lyrics-api'
+// @ts-ignore
 import YouTubePlayer from 'youtube-player'
-import logo from './logo.svg'
 import './App.scss'
+
+import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai'
+import { IoMdRefresh } from 'react-icons/io'
 
 export type IPage = 'coin_insert' | 'song_select' | 'song_player'
 
@@ -67,6 +71,16 @@ export interface Song {
 export function GetAllSongs () {
   const songs: Song[] = []
   songs.push({
+    title: 'Omoide Shiritori',
+    artist: 'DIALOGUE+',
+    id: 'DvAi0IKt9Ho'
+  })
+  songs.push({
+    title: 'Jadikan Aku Yang Kedua',
+    artist: 'Astrid',
+    id: 'biMXsluCNnE'
+  })
+  songs.push({
     title: 'Heavy Rotation',
     artist: 'AKB48',
     id: 'CSPw5PK8zTg'
@@ -104,12 +118,16 @@ export function GetAllSongs () {
   return songs  
 }
 
+export type IPageSongPlayerState = 'pause' | 'play' | 'stop'
+
 export function PageSongPlayer({ song, onBack }: { song: Song, onBack: () => void }) {
   const [player, setPlayer] = useState<any>()
+  const [lyric, setLyric] = useState<any>()
   const [lastState, setLastState] = useState<number>()
   const [started, setStarted] = useState(false)
   const [duration, setDuration] = useState(0)
   const [currTime, setCurrTime] = useState(0)
+  const [state, setState] = useState<IPageSongPlayerState>('stop')
   const displayDuration = (duration: number) => {
     const minutes = Math.floor(duration / 60)
     const seconds = duration % 60
@@ -118,6 +136,10 @@ export function PageSongPlayer({ song, onBack }: { song: Song, onBack: () => voi
   const onPlayerStarted = (player: any) => {
     setTimeout(() => {
       player.getDuration().then((dur: any) => setDuration(Math.round(dur)))
+      const lyrics = new Lyrics();
+      console.log(lyrics.getLyrics('Hot N Cold').then((a: any) => {
+        console.log(a)
+      }))
     }, 1000)
   }
   const onPlayerStateChange = (player: any, event: any) => {
@@ -127,14 +149,21 @@ export function PageSongPlayer({ song, onBack }: { song: Song, onBack: () => voi
     }
   }
   useEffect(() => {
-    const player = YouTubePlayer('video-player')
+    const player = YouTubePlayer('video-player', {
+      width: '100%',
+    })
     setPlayer(player)
     player.loadVideoById(song.id)
     player.playVideo().then(() => {
       onPlayerStarted(player)
+      const lyricAppId = 'frM-Ihf1DeEorOpUuOQ5GY4_a3KedTBoSgfH7C1DtGWVDWxzw06_EOSI6sXsXBCw'
+      const lyricAppSecret = 'ZxE4XjCvvsnM6JcatpTqShVM1G018FBgGUcfeiylB9TKQxdZ8slnkBmVZi86kviPjqHLjU0aSFRX3HMYX4Y8-A'
+
     })
     player.on('stateChange', (event: any) => {
+      console.log('stateChange', event)
       if (!started && event.data === 3) {
+        setState('play')
         setStarted(true)
       }
       onPlayerStateChange(player, event)
@@ -154,6 +183,29 @@ export function PageSongPlayer({ song, onBack }: { song: Song, onBack: () => voi
       }
     }
   }, [])
+
+  // methods
+  const reset = () => {
+    try {
+      player.stopVideo().then(() => {
+        setState('stop')
+        setStarted(false)
+        play()
+      })
+    } catch (error) {}
+  }
+  const play = () => {
+    try {
+      player.playVideo()
+      setState('play')
+    } catch (error) {}
+  }
+  const pause = () => {
+    player.pauseVideo()
+    setState('pause')
+  }
+
+
   return (
     <div className="flex-1 font-bold text-2xl flex flex-col space-y-2 relative overflow-hidden p-4 bg-slate-900">
       <div>
@@ -162,19 +214,46 @@ export function PageSongPlayer({ song, onBack }: { song: Song, onBack: () => voi
         </button>
       </div>
       <div className="flex-1">
-        <div className="">
+        <div className="relative overflow-hidden">
           <div className="video-player" id="video-player" />
+          <div
+            className="bg-slate-700/50 top-0 left-0 w-full h-full absolute z-10"
+          />
         </div>
         <div className="mt-4">
-          <div className="music-bar" id="progress">
+          <div className="music-bar mb-4" id="progress">
             <div
               id="length"
               style={{
                 width: `${(currTime / duration) * 100}%`
-              }}></div>
+              }}
+            />
           </div>
-          <div className="text-center">
+          <div className="text-center text-sm mb-4">
             {displayDuration(currTime)} / {displayDuration(duration)}
+          </div>
+          <div className="w-full justify-center flex space-x-1">
+            <button
+              className="text-white px-2 py-1 rounded bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:text-slate-700"
+              onClick={reset}
+              disabled={state === 'stop'}
+            >
+              <IoMdRefresh />
+            </button>
+            <button
+              className="text-white px-2 py-1 rounded bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:text-slate-700"
+              onClick={play}
+              disabled={state === 'play'}
+            >
+              <AiFillPlayCircle />
+            </button>
+            <button
+              className="text-white px-2 py-1 rounded bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:text-slate-700"
+              onClick={pause}
+              disabled={state === 'pause'}
+            >
+              <AiFillPauseCircle />
+            </button>
           </div>
         </div>
       </div>
@@ -190,18 +269,30 @@ export function PageSelectSong({
   onSelectSong: (song: Song) => void
 }) {
   const songs = GetAllSongs()
+  const [filterText, setFilterText] = useState('')
+  const songsFiltered = () => songs.filter(song => {
+    const titleMatch = song.title.toLowerCase().includes(filterText.toLowerCase())
+    const artistMatch = song.artist.toLowerCase().includes(filterText.toLowerCase())
+    return titleMatch || artistMatch
+  })
   return (
     <div className="flex-1 font-bold text-2xl flex flex-col space-y-2 relative overflow-hidden p-4 bg-slate-900">
       <div className="invisible">
         <div className="video-player" id="video-player" />
       </div>
-      <div className="flex justify-end space-x-2">
+      <div className="flex justify-between space-x-2 items-center">
+        <input
+          type="text"
+          className="rounded-lg px-2 py-1 text-xs bg-slate-600"
+          placeholder="Search Song..."
+          onChange={(e) => setFilterText(e.target.value)}
+        />
         <div className="rounded px-4 py-2 text-xs bg-blue-500">
           Remaining Song : {songCount + 2}
         </div>
       </div>
-      <div className="flex-1 flex flex-col space-y-2">
-        {songs.map((song, index) => (
+      <div className="flex-1 flex flex-col space-y-2 overflow-y-auto">
+        {songsFiltered().map((song, index) => (
           <div
             className="cursor-pointer px-4 py-2 rounded bg-slate-800 hover:bg-slate-700"
             key={index}
@@ -222,6 +313,7 @@ function App() {
   const [page, setPage] = useState<IPage>('coin_insert')
   const [songCount, setSongCount] = useState(0)
   const [song, setSong] = useState<Song>()
+  const isMobile = () => window.innerWidth < 768
   useEffect(() => {
     if (page === 'song_select' || page === 'song_player') {
       setSongCount(songCount - 1)
@@ -236,8 +328,8 @@ function App() {
         <div
           className="flex flex-col w-96 h-96 bg-red-700 rounded-xl relative overflow-hidden"
           style={{
-            width: '40rem',
-            height: '40rem',
+            width: isMobile() ? '100%' : '40rem',
+            height: isMobile() ? '100%' : '40rem',
           }}
         >
           <div className="bg-slate-800">
@@ -245,7 +337,7 @@ function App() {
               KARAOKE-CHAN
             </div>
           </div>
-          <div className="flex-1 flex flex-col items-center justify-between p-6 bg-slate-700">
+          <div className="flex-1 flex flex-col items-center justify-between p-6 bg-slate-700 overflow-hidden">
             <div className="w-full flex-1 flex flex-col rounded-xl relative overflow-hidden bg-slate-900">
               {page === 'coin_insert' && <PageCoinInsert onContinue={(songCount: number) => {
                 setPage('song_select')
